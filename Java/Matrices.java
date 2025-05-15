8812cd9beacd29b7ab8655b7478859e15f883be8
Steven Cardona <stevencardona2001@gmail.com>
2025-05-15 11:15:54 -0500

    Update Matrices.java

--- Java/Matrices.java
+++ Java/Matrices.java
@@ -1,9 +1,9 @@
 import javax.swing.JOptionPane;
 
-public class Matrices {
+public class matrices {
     public static void main(String[] args){
         int mat[][]={{6,7,4,9,10,4},{10,74,30,60,8,2},{7,20,90,100,45,32},{3,1,7,13,21,29},{19,27,32,47,27,3},{8,7,20,87,45,30}};
-        diagonalprincipal(mat);
+        culebrita_horizontal(mat);
     }
 
      /*public static void mostrarmatriz(int mat[][]){
@@ -17,7 +17,7 @@ public class Matrices {
             matriz = matriz + "\n";
         }
         JOptionPane.showMessageDialog(null,matriz);
-    }*/
+    }
 
     public static void diagonalprincipal(int[][] mat){
         String matriz=" ";
@@ -27,6 +27,25 @@ public class Matrices {
                 if(f==c){
                 matriz = matriz + mat[f][c]+" ";
                 }
+            }
+            matriz = matriz + "\n";
+        }
+        JOptionPane.showMessageDialog(null,matriz);
+
+    }*/
+
+    public static void culebrita_horizontal(int[][] mat){
+        String matriz=" ";
+        int filas = mat.length;
+        for(int f=0;f<filas;f++){
+            if(f % 2 == 0){
+                for(int c=0;c<mat[f].length;c++){
+                    matriz = matriz + mat[f][c]+" ";
+                    }
+            }else{
+                for(int c=filas;c>0;c-=1){
+                    matriz = matriz + mat[f][c]+" ";
+                    }
             }
             matriz = matriz + "\n";
         }
