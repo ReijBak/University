f99bdc9398fd7f15f7ea0eed4da1e71bd1671432
Steven Cardona <stevencardona2001@gmail.com>
2025-05-15 11:38:38 -0500

    Update Matrices.java

--- Java/Matrices.java
+++ Java/Matrices.java
@@ -1,25 +1,27 @@
 import javax.swing.JOptionPane;
 
 public class matrices {
+    static String matriz1 = " ";
     public static void main(String[] args){
         int mat[][]={{6,7,4,9,10,4},{10,74,30,60,8,2},{7,20,90,100,45,32},{3,1,7,13,21,29},{19,27,32,47,27,3},{8,7,20,87,45,30}};
+        mostrarmatriz(mat);
         culebrita_horizontal(mat);
     }
 
-     /*public static void mostrarmatriz(int mat[][]){
+    public static void mostrarmatriz(int mat[][]){
 
-        String matriz=" ";
+        
         int filas = mat.length;
         for(int f=0;f<filas;f++){
             for(int c=0;c<mat[f].length;c++){
-                matriz = matriz + mat[f][c]+" ";
+                matriz1 = matriz1 + mat[f][c]+" ";
             }
-            matriz = matriz + "\n";
+            matriz1 = matriz1 + "\n";
         }
-        JOptionPane.showMessageDialog(null,matriz);
+       /* JOptionPane.showMessageDialog(null,matriz); */
     }
 
-    public static void diagonalprincipal(int[][] mat){
+     /*public static void diagonalprincipal(int[][] mat){
         String matriz=" ";
         int filas = mat.length;
         for(int f=0;f<filas;f++){
@@ -43,13 +45,13 @@ public class matrices {
                     matriz = matriz + mat[f][c]+" ";
                     }
             }else{
-                for(int c=filas-1;c=>0;c--){
-                    matr= iz matriz + mat[f][c]+" ";
+                for(int c=filas-1;c>=0;c-=1){
+                    matriz = matriz + mat[f][c]+" ";
                     }
             }
             matriz = matriz + "\n";
         }
-        JOptionPane.showMessageDialog(null,matriz);
+        JOptionPane.showMessageDialog(null, matriz1 +"\n"+ matriz);
 
     }
 
