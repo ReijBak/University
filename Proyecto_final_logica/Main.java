19f272a3fc573a32b6492cc44f672c900bb1df81
Steven Cardona <stevencardona2001@gmail.com>
2025-06-01 13:32:30 -0500

    Add files via upload

---
+++ Proyecto_final_logica/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Torneo torneo = new Torneo();
+        torneo.registrarEquipos();
+        torneo.etapaTodosContraTodos();
+        torneo.etapaFaseDeGrupos();
+    }
+}
