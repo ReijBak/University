828fba4ddafc691176b590e0bca5f76ce2d3fb12
Steven Cardona <stevencardona2001@gmail.com>
2025-06-02 22:12:03 -0500

    Delete Proyecto_final_logica/Proyecto_final_logica directory

--- Proyecto_final_logica/Proyecto_final_logica/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Torneo torneo = new Torneo();
-        torneo.registrarEquipos();
-        torneo.etapaTodosContraTodos();
-        torneo.etapaFaseDeGrupos();
-    }
-}
